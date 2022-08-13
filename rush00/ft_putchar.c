da4151f7f87e0f53812440ccc49342c654b87961
Sol Nicole Andrade <soandrad@c2r10s1.42malaga.com>
2022-08-13 14:02:43 +0200

    added rush00 & c00

--- rush00/ft_putchar.c
+++ rush00/ft_putchar.c
@@ -1,41 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*   ft_putchar.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: soandrad <soandrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/11 15:49:44 by soandrad          #+#    #+#             */
-/*   Updated: 2022/08/13 13:15:48 by soandrad         ###   ########.fr       */
+/*   Created: 2022/08/13 10:57:11 by soandrad          #+#    #+#             */
+/*   Updated: 2022/08/13 13:48:52 by soandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 
-void	ft_putchar(char c)
+int	ft_putchar(char c)
 {
 	write(1, &c, 1);
-}
-
-void	ft_print_comb()
-{
-    int n1 = 0;
-    int n2 = 0;
-    int n3 = 0;
-	while(n1<=9){
-        while(n2<=9){
-            while(n3<=9){ 
-                printf(n1 + n2 +q n3);
-                n3++;
-            }
-            n2++;
-        }
-        n1++;
-    }
-    
-}
-int main(){
-    ft_print_comb();
-    return(0);
+	return (0);
 }
